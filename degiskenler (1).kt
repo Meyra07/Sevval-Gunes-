795a671a0b671ece2a689fe7a45613f38f14abe0
Meyra07 <meyra6460@gmail.com>
2025-04-28 22:57:00 +0300

    Şevval Güneş
    degiskenler.kt

---
+++ degiskenler (1).kt
@@ -0,0 +1,96 @@
+package degiskenler
+
+class degiskenler {
+}
+
+val sehirAdi: String = "Milano"
+
+val ulke: String = "İtalya"
+
+var Telefon: Long = 998500034127
+
+val Postakodu: Int = 24050
+
+var email: String = "jamescotlin@gmail.com"
+
+var meslek: String = "Doctor"
+
+var stokMiktari: Byte = 125
+
+val musteriAdi: String = "Ahmet"
+
+var bakiye: Short = 30000
+
+var dogumGunu: String = "Mart"
+
+var maas: Long = 17000
+
+var medeniDurum: String = "Kibar"
+
+var urunYorum: String = "Herkese tavsiye ederim fiyatını hak ediyor"
+
+var odemeTarihi: Double = 01.25
+
+var odeme: Int = 2000
+
+var siparisAdedi: Int = 5
+
+val arabaModeli: String = "MercedesBenz"
+
+var kitapAdi: String = "SendenBirtaneDahaYok"
+
+var yayınlamaTarihi: Double = 07.02
+
+var indirimmiktari: Int = 25
+
+var odasayisi: Int = 3+1
+
+var enlem: Int = 25
+
+var boylam: Int = 35
+
+val urunAdi: String = "Zara"
+
+var yemekFiyati: Int = 150
+
+val marka: String = "Lacoste"
+
+val muzikAdi: String = "Self Control"
+
+var vidioSuresi: Double = 32.05
+
+var urunPuani: Int = 10
+
+val resimAdi: String = "Monalisa"
+
+val dosyaFormati: String = "exe"
+
+val renk: String = "Siyah"
+
+var renkKodu: Int = 22
+
+var telefonModeli: String = "iphone"
+
+var ekranBoyutu: Double = 17.2
+
+var agirlik: Int = 65
+
+val ulusalGun: String = "15 Temmuz"
+
+val tatilGunu: String = "5 Agustos"
+
+var rezarvasyonTarihi: Double = 07.25
+
+val sokakAdi: String = "Necmiye Sokak"
+
+var otobusHatti: Long = 4450525
+
+var kalanDakika: Int = 45
+
+var takipKodu: Long = 99163809512
+
+val kuponSuresi: Double = 22.25
+
+val kuponKodu: String = "XXTW8JUY95"
+
+var faturaAdresi: String = "Mimarsinan no.52 İstanbul"
